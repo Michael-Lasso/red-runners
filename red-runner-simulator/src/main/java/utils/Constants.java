d9a5ca77983c29c6824eb6f2c6f8416a995266d8
Michael Lasso <lassomicha@hotmail.com>
2016-11-03 20:52:17 -0400

    Stable branch, renamed projects and ready for development

--- red-runner-simulator/src/main/java/utils/Constants.java
+++ red-runner-simulator/src/main/java/utils/Constants.java
@@ -4,11 +4,11 @@ public class Constants {
 	public static final String LONGITUDE = "-105.240976";
 	public static final String LATITUDE = "40.018301";
 	public static final String SEPARATOR = ":";
-
-	public static final String TRAFFIC_FILE_PATH = "/Users/michaellasso/Documents/hackaton/make-haven/traffic_light.txt";
-	public static final String CHECKER_FILE_PATH = "/Users/michaellasso/Documents/hackaton/make-haven/checker.sh";
-	public static final String TIMESTAMP_FILE_PATH = "/Users/michaellasso/Documents/hackaton/make-haven/timestamp.txt";
-	public static final String COORDS_FILE_PATH = "/Users/michaellasso/Documents/hackaton/make-haven/coordinates.txt";
+	public static final String MAIN_FILE_PATH = "/Users/michaellasso/Documents/hackatons/red-runners/resources/";
+	public static final String TRAFFIC_FILE_PATH = MAIN_FILE_PATH + "traffic_light.txt";
+	public static final String CHECKER_FILE_PATH = MAIN_FILE_PATH + "checker.sh";
+	public static final String TIMESTAMP_FILE_PATH = MAIN_FILE_PATH + "timestamp.txt";
+	public static final String COORDS_FILE_PATH = MAIN_FILE_PATH + "coordinates.txt";
 
 	public static final String API_KEY = "uKpKvdPt9KDeb3b6DDEVo8S5QaiS1PmW";
 	public static final String SECRET = "ZYWf6F6AqxGZvWFh";
